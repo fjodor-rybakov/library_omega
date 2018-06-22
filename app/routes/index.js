c3b026afa25e9fc48019bda620ee1a927a82fd40
fjodor-rybakov <fedryb@mail.ru>
2018-06-22 22:52:38 +0300

    add new exept

--- app/routes/index.js
+++ app/routes/index.js
@@ -12,10 +12,30 @@ module.exports = function (app, db) { // методы post/get
 		return (num ^ 0) === num;
 	}
 
+	function isset() {
+		var a = arguments, l = a.length, i = 0;
+    	if (l === 0) {
+    		throw new Error('Empty isset');
+    	}
+
+    	while (i !== l) {
+	        if (typeof(a[i]) == 'undefined' || a[i] === null) {
+	            return false;
+	        } else {
+	            i++;
+	        }
+	    }
+
+	    return true;
+	}
+
     app.post('/addBook', (req, res) => { // добавление книги
     	try {
+    		if (!isset(req.body.name, req.body.link, req.body.author_name, req.body.description, req.body.year)) 
+    			throw new Error("Not enough body data: mast be (name, link, author_namem description, year)");
+
     		let year_book = +req.body.year;
-    		if (!isNumeric(year_book) || !isInteger(year_book)) throw new Error("year mast be numeric and integer");
+    		if (!isNumeric(year_book) || !isInteger(year_book)) throw new Error("Year mast be numeric and integer");
 
     		let book = {
 	            name: req.body.name,
@@ -40,6 +60,7 @@ module.exports = function (app, db) { // методы post/get
 
     app.get('/books/:id', (req, res) => { // получить информацию о книге по id
     	try {
+    		if (!isset(req.params.id)) throw new Error("Not enough parameters: mast be (id)");
     		collectionBook.findOne({"_id": ObjectID(req.params.id)}, function (err, info) {
 	            if (err) throw err;
 	            res.send(info);
@@ -52,6 +73,8 @@ module.exports = function (app, db) { // методы post/get
 
     app.post('/books/:id', (req, res) => { // забронировать книгу на 10 дней
     	try {
+    		if (!isset(req.params.id)) throw new Error("Not enough parameters: mast be (id)");
+    		if (!isset(req.body.name)) throw new Error("Not enough body data: mast be (name)");
     		const ms = 86400000;
 	        const book_id = ObjectID(req.params.id);
 	        let date = new Date();
@@ -84,10 +107,13 @@ module.exports = function (app, db) { // методы post/get
 
     app.get('/showPage/:numPage', (req, res) => { // пейджинг
     	try {
+    		if (!isset(req.params.numPage)) throw new Error("Not enough parameters: mast be (numPage)");
+
     		let numPage = +req.params.numPage;
     		let countPage = 20;
-    		if (!isNumeric(numPage) || !isInteger(numPage)) throw new Error("number page mast be numeric and integer");
-    		if (numPage < 1) throw new Error("argument less 1");
+
+    		if (!isNumeric(numPage) || !isInteger(numPage)) throw new Error("Number page mast be numeric and integer");
+    		if (numPage < 1) throw new Error("Argument less 1");
 
     		collectionBook.find().limit(countPage * numPage).toArray(function(err, results) {
     			if (err) throw err;
@@ -101,6 +127,8 @@ module.exports = function (app, db) { // методы post/get
 
     app.get('/searchBook/:substring', (req, res) => { // поиск по подстроке (фильтрация по названию)
     	try {
+    		if (!isset(req.params.substring)) throw new Error("Not enough parameters: mast be (substring)");
+
     		let substring = req.params.substring;
 	    	let query = { 
 	    		"name": {$regex: substring} 
@@ -118,6 +146,8 @@ module.exports = function (app, db) { // методы post/get
 
     app.get('/book', (req, res) => { // фильтрация по статусу
     	try {
+    		if (!isset(req.query.available)) throw new Error("Not enough query parameters: mast be (available)");
+
     		let available = req.query.available;
 	    	let isAvailable = (available == 'true'); // прости, но ты пишешь на javascript 
 	    	let query = (available ? {"available": isAvailable} : {});
