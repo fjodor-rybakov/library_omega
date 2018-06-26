8ba9b8edaa352959d01ef0c3ad37c148778a0ae6
lezhnina-anna <Ankemova@gmail.com>
2018-06-26 16:17:00 +0300

    added function to return book

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,154 +2,188 @@ var errs = require('restify-errors');
 const ObjectID = require('mongodb').ObjectID;
 
 module.exports = function (app, db) { // методы post/get
-	const dbName = db.db("library-omega");
-	const collectionBooking = dbName.collection("booking");
-	const collectionBook = dbName.collection("book");
+    const dbName = db.db("library-omega");
+    const collectionBooking = dbName.collection("booking");
+    const collectionBook = dbName.collection("book");
 
-	function isNumeric(num) {
-		return !isNaN(parseFloat(num)) && isFinite(num);
-	}
+    function isNumeric(num) {
+        return !isNaN(parseFloat(num)) && isFinite(num);
+    }
 
-	function isInteger(num) {
-		return (num ^ 0) === num;
-	}
+    function isInteger(num) {
+        return (num ^ 0) === num;
+    }
 
-	function isset() {
-		var a = arguments, l = a.length, i = 0;
-		if (l === 0) {
-			console.log('Empty isset');
-		}
+    function isset() {
+        var a = arguments, l = a.length, i = 0;
+        if (l === 0) {
+            console.log('Empty isset');
+        }
 
-		while (i !== l) {
-			if (typeof(a[i]) == 'undefined' || a[i] === null) {
-				return false;
-			} else {
-				i++;
-			}
-		}
+        while (i !== l) {
+            if (typeof(a[i]) == 'undefined' || a[i] === null) {
+                return false;
+            } else {
+                i++;
+            }
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	app.post('/books', (req, res, next) => { // добавление книги
-		if (!isset(req.body.name, req.body.link, req.body.authors, req.body.description, req.body.year))
-			return next(new errs.InvalidArgumentError("Not enough body data: mast be (name, link, authors, description, year)"));
+    app.post('/books', (req, res, next) => { // добавление книги
+        if (!isset(req.body.name, req.body.link, req.body.authors, req.body.description, req.body.year))
+            return next(new errs.InvalidArgumentError("Not enough body data: mast be (name, link, authors, description, year)"));
 
-		let year_book = +req.body.year;
+        let year_book = +req.body.year;
 
-		if (!isNumeric(year_book) || !isInteger(year_book))
-			return next(new errs.InvalidArgumentError("Year mast be numeric and integer"));
+        if (!isNumeric(year_book) || !isInteger(year_book))
+            return next(new errs.InvalidArgumentError("Year mast be numeric and integer"));
 
-		let book = {
-			name: req.body.name,
-			link: req.body.link,
-			authors: req.body.authors,
-			available: true,
-			description: req.body.description,
-			year: year_book
-		};
+        let book = {
+            name: req.body.name,
+            link: req.body.link,
+            authors: req.body.authors,
+            available: true,
+            description: req.body.description,
+            year: year_book
+        };
 
-		collectionBook.insertOne(book, function (err, result) {
-			if (err)
-				return next(new errs.BadGatewayError(err.message));
-			console.log(result.ops);
-		});
+        collectionBook.insertOne(book, function (err, result) {
+            if (err)
+                return next(new errs.BadGatewayError(err.message));
+            console.log(result.ops);
+        });
 
-		res.send("Book successfully added");
-	});
+        res.send("Book successfully added");
+    });
 
-	app.get('/books/:id', (req, res, next) => { // получить информацию о книге по id
-		if (!isset(req.params.id)) 
-			return next(new errs.InvalidArgumentError("Not enough parameters: mast be (id)"));
+    app.get('/books/:id', (req, res, next) => { // получить информацию о книге по id
+        if (!isset(req.params.id))
+            return next(new errs.InvalidArgumentError("Not enough parameters: mast be (id)"));
 
-		collectionBook.findOne({"_id": ObjectID(req.params.id)}, function (err, info) {
-			if (err) 
-				return next(new errs.BadGatewayError(err.message));
-			res.send(info);
-		});
-	});
+        collectionBook.findOne({"_id": ObjectID(req.params.id)}, function (err, info) {
+            if (err)
+                return next(new errs.BadGatewayError(err.message));
+            res.send(info);
+        });
+    });
 
-	app.post('/booking', (req, res, next) => { // забронировать книгу на 10 дней
-		if (!isset(req.body.id))
-			return next(new errs.InvalidArgumentError("Not enough body data: mast be (id)"));
-		if (!isset(req.body.name)) 
-			return next(new errs.InvalidArgumentError("Not enough body data: mast be (name)"));
+    app.post('/booking', (req, res, next) => { // забронировать книгу или снять бронь
 
-		const ms = 86400000;
-		const book_id = ObjectID(req.body.id);
-		let date = new Date();
+        if (!isset(req.body.id))
+            return next(new errs.InvalidArgumentError("Not enough body data: mast be (id)"));
+        if (!isset(req.body.name))
+            return next(new errs.InvalidArgumentError("Not enough body data: mast be (name)"));
+        if (!isset(req.body.available))
+            return next(new errs.InvalidArgumentError("Not enough body data: mast be (available)"));
 
-		let booking = {
-			book_id: book_id,
-			user: req.body.name,
-			taken: date,
-			returned: new Date(date.getTime() + ms * 10)
-		};
+        const book_id = ObjectID(req.body.id);
+        let date = new Date();
 
-		collectionBooking.insertOne(booking, function (err) {
-			if (err) 
-				return next(new errs.BadGatewayError(err.message));
-		});
+        if (req.body.available === 'true') //забронировать
+        {
+            let booking = {
+                book_id: book_id,
+                user: req.body.name,
+                taken: date,
+                returned: null
+            };
 
-		let query = {_id: book_id};
-		let values = {$set: {available: false}};
+            collectionBooking.insertOne(booking, function (err) {
+                if (err)
+                    return next(new errs.BadGatewayError(err.message));
+            });
 
-		collectionBook.updateOne(query, values, function (err) {
-			if (err) 
-				return next(new errs.BadGatewayError(err.message));
-		});
+            let query = {_id: book_id};
+            let values = {$set: {available: false}};
 
-		res.send("Book successfully booked");
-	});
+            collectionBook.updateOne(query, values, function (err) {
+                if (err)
+                    return next(new errs.BadGatewayError(err.message));
+            });
+
+            res.send("Book successfully booked");
+
+        } else { // снять бронь
+
+            let query = {$and: [{book_id: book_id}, {user: req.body.name}]};
+
+            collectionBooking.find(query).sort({taken: -1}).toArray(function (err, result) {
+                if (err || result.length === 0)
+                    return next(new errs.BadGatewayError(err.message));
+
+                let query = {_id: book_id};
+                let values = {$set: {available: true}};
+
+                collectionBook.updateOne(query, values, function (err) {
+                    if (err)
+                        return next(new errs.BadGatewayError(err.message));
+                });
+
+                let idBooking = result[0]._id;
+
+                query = {_id: idBooking};
+                values = {$set: {returned: date}};
+
+                collectionBooking.updateOne(query, values, function (err) {
+                    if (err)
+                        return next(new errs.BadGatewayError(err.message));
+                });
+
+                res.send("Book was returned");
+            });
+        }
+    });
 
 
-	app.get('/books/showPage/:numPage', (req, res, next) => { // пейджинг
-		if (!isset(req.params.numPage)) 
-			return next(new errs.InvalidArgumentError("Not enough parameters: mast be (numPage)"));
+    app.get('/books/showPage/:numPage', (req, res, next) => { // пейджинг
+        if (!isset(req.params.numPage))
+            return next(new errs.InvalidArgumentError("Not enough parameters: mast be (numPage)"));
 
-		let numPage = +req.params.numPage;
-		let countBook = 20;
+        let numPage = +req.params.numPage;
+        let countBook = 20;
 
-		if (!isNumeric(numPage) || !isInteger(numPage)) 
-			return next(new errs.InvalidArgumentError("Number page mast be numeric and integer"));
-		if (numPage < 1) 
-			return next(new errs.InvalidArgumentError("Argument less 1"));
+        if (!isNumeric(numPage) || !isInteger(numPage))
+            return next(new errs.InvalidArgumentError("Number page mast be numeric and integer"));
+        if (numPage < 1)
+            return next(new errs.InvalidArgumentError("Argument less 1"));
 
-		collectionBook.find().limit(countBook).skip(countBook * (numPage - 1)).toArray(function(err, results) {
-			if (err) 
-				return next(new errs.BadGatewayError(err.message));
-			res.send(results);
-		});
-	});
+        collectionBook.find().limit(countBook).skip(countBook * (numPage - 1)).toArray(function (err, results) {
+            if (err)
+                return next(new errs.BadGatewayError(err.message));
+            res.send(results);
+        });
+    });
 
-	app.get('/books/searchBook', (req, res, next) => { // поиск по подстроке (фильтрация по названию)
-		if (!isset(req.query.substring)) 
-			return next(new errs.InvalidArgumentError("Not enough query parameters: mast be (substring)"));
+    app.get('/books/searchBook', (req, res, next) => { // поиск по подстроке (фильтрация по названию)
+        if (!isset(req.query.substring))
+            return next(new errs.InvalidArgumentError("Not enough query parameters: mast be (substring)"));
 
-		let substring = req.query.substring;
-		let query = { 
-			"name": {$regex: substring} 
-		};
+        let substring = req.query.substring;
+        let query = {
+            "name": {$regex: substring}
+        };
 
-		collectionBook.find(query).toArray(function(err, result) {
-			if (err) 
-				return next(new errs.BadGatewayError(err.message));
-			res.send(result);
-		});
-	});
+        collectionBook.find(query).toArray(function (err, result) {
+            if (err)
+                return next(new errs.BadGatewayError(err.message));
+            res.send(result);
+        });
+    });
 
-	app.get('/books', (req, res, next) => { // фильтрация по статусу
-		if (!isset(req.query.available)) 
-			return next(new errs.InvalidArgumentError("Not enough query parameters: mast be (available)"));
+    app.get('/books', (req, res, next) => { // фильтрация по статусу
+        if (!isset(req.query.available))
+            return next(new errs.InvalidArgumentError("Not enough query parameters: mast be (available)"));
 
-		let available = req.query.available;
-		let isAvailable = (available === 'true'); // прости, но ты пишешь на javascript
-		let query = (available ? {"available": isAvailable} : {});
+        let available = req.query.available;
+        let isAvailable = (available === 'true'); // прости, но ты пишешь на javascript
+        let query = (available ? {"available": isAvailable} : {});
 
-		collectionBook.find(query).toArray(function(err, result) {
-			if (err) 
-				return next(new errs.BadGatewayError(err.message));
-			res.send(result);
-		});
-	});
+        collectionBook.find(query).toArray(function (err, result) {
+            if (err)
+                return next(new errs.BadGatewayError(err.message));
+            res.send(result);
+        });
+    });
 };
