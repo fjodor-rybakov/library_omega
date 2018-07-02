2858335542ba9ba942e9d62baf155f1f7f3055d5
fjodor-rybakov <fedryb@mail.ru>
2018-07-02 23:07:42 +0300

    fix test

--- test/appTests.js
+++ test/appTests.js
@@ -1,6 +1,8 @@
 var supertest = require('supertest');
 var assert = require('assert');
-var app = require('../index');
+//var app = require('../index');
+
+var url = 'https://libraryomega.herokuapp.com';
 
 /*
 var s;
@@ -10,12 +12,11 @@ s = app.listen(function() {
 	});
 */
 
-/*describe('addBook', function() {
+/*describe('add book', function() {
 	it('mustAdd', function(done) {
 		app.listen(function() {
-			var url = 'http://localhost:' + 3000;
 			supertest(url)
-			.post('/addBook')
+			.post('/books')
 			.send({name: 'john',
 				link: 'link',
 				authors: 'author_name',
@@ -24,89 +25,85 @@ s = app.listen(function() {
 			.set('Content-Type', 'application/json')
 			.set('Accept', 'application/json')
 			.expect(200, function(err,result) {
-				console.log(err);
-				console.log(JSON.stringify(result));
-				if (err) throw err;
+				if (err) {
+					console.log(err);
+					console.log(JSON.stringify(res));
+					throw err;
+				}
 				done();
 			});
 		});
 	});
-});
-
-describe('bookInfo', function () {
-	it('getInfoInJSON', function (done) {
-		app.listen(function() {
-			var url = 'http://localhost:' + 3000;
-			supertest(url)
-			.get('/books/5b2e32f9fb6fc048e105b472')
-			.expect('Content-Type', /json/)
-			.expect(200, function (err,res) {
-				console.log(err);
-				console.log(JSON.stringify(res));
-				if (err) throw err;
-				done();
-			});
-		});
-	})
 });*/
 
-/*describe('paging books', function () {
+describe('get book info', function () {
+	it('getInfoInJSON', function (done) {
+			supertest(url)
+			.get('/books/5b39040051c1b60d14738c7a')
+			.expect('Content-Type', /json/)
+			.expect(200, function (err,res) {
+				if (err) {
+					console.log(err);
+					console.log(JSON.stringify(res));
+					throw err;
+				}
+				done();
+			});
+	})
+});
+
+describe('paging books', function () {
 	it('paging', function (done) {
-		app.listen(function() {
-			var url = 'http://localhost:' + 3000;
 			supertest(url)
 			.get('/books/showPage/1')
 			.expect('Content-Type', /json/)
 			.expect(200, function (err,res) {
-				console.log(err);
-				console.log(JSON.stringify(res));
-				if (err) throw err;
+				if (err) {
+					console.log(err);
+					console.log(JSON.stringify(res));
+					throw err;
+				}
 				done();
 			});
-		});
 	})
 });
 
 describe('search substring books', function() {
 	it('search substring', function(done) {
-		app.listen(function() {
-			var url = 'http://localhost:' + 3000;
 			supertest(url)
 			.get('/books/searchBook')
-			.query({ substring: 'new' })
+			.query({ substring: 'Б' })
 			.expect('Content-Type', /json/)
 			.expect(200, function (err,res) {
-				console.log(err);
-				console.log(JSON.stringify(res));
-				if (err) throw err;
+				if (err) {
+					console.log(err);
+					console.log(JSON.stringify(res));
+					throw err;
+				}
 				done();
 			});
-		});
 	});
 });
 
 describe('filter status books', function() {
 	it('filter status', function(done) {
-		app.listen(function() {
-			var url = 'http://localhost:' + 3000;
 			supertest(url)
 			.get('/books')
 			.query({ available: true })
 			.expect('Content-Type', /json/)
 			.expect(200, function (err,res) {
-				console.log(err);
-				console.log(JSON.stringify(res));
-				if (err) throw err;
+				if (err) {
+					console.log(err);
+					console.log(JSON.stringify(res));
+					throw err;
+				}
 				done();
 			});
-		});
 	});
-});*/
+});
 
 
-describe('booking', function () {
-    var url = 'http://localhost:' + 3000;
-
+/*describe('booking', function () {
     it('getInfoInJSON', function (done) {
         app.listen(function() {
             supertest(url)
@@ -117,9 +114,11 @@ describe('booking', function () {
                     available: 'true'
                 })
                 .expect(200, function (err,res) {
-                    console.log(err);
-                    console.log(JSON.stringify(res));
-                    if (err) throw err;
+                    if (err) {
+						console.log(err);
+						console.log(JSON.stringify(res));
+						throw err;
+					}
                     done();
                 });
         });
@@ -133,12 +132,14 @@ describe('booking', function () {
                 .expect(200, function (err,res) {
                     var data = JSON.parse(res.text);
                     //console.log(data.available);
-                    console.log(err);
-                    console.log(JSON.stringify(res));
-                    if (err) throw err;
+                    if (err) {
+						console.log(err);
+						console.log(JSON.stringify(res));
+						throw err;
+					}
                     assert.strictEqual(data.available, false);
                     done();
                 })
         })
     })
-});
+});*/
