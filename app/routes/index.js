66722ce804d936dcc660a8d39521186aa1a4f586
fjodor-rybakov <fedryb@mail.ru>
2018-07-02 13:58:37 +0300

    refact modules

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,12 +1,12 @@
-var errs = require('restify-errors');
-const ObjectID = require('mongodb').ObjectID;
+const errs = require('restify-errors'),
+	  ObjectID = require('mongodb').ObjectID;
 
 let message = 'This library app. accept requests: /books - добавление книги (post, необходимые параметры в body: name, link, authors, description, year); /books/:id - получить информацию о книге по id (get) /booking - забронировать книгу или снять бронь (post, необходимые параметры в body: id, name, available); /books/showPage/:numPage - пейджинг (get); /books/searchBook - поиск по подстроке (фильтрация по названию) (get, query запрос необходимые параметры: substring); /books - фильтрация по статусу (get, query запрос необходимые параметры: available);';
 
-module.exports = function (app, db) { // методы post/get
-	const dbName = db.db("library-omega");
-	const collectionBooking = dbName.collection("booking");
-	const collectionBook = dbName.collection("book");
+module.exports = (app, db) => { // методы post/get
+	const dbName = db.db("library-omega"),
+		  collectionBooking = dbName.collection("booking"),
+		  collectionBook = dbName.collection("book");
 
 	function isNumeric(num) {
 		return !isNaN(parseFloat(num)) && isFinite(num);
@@ -35,6 +35,7 @@ module.exports = function (app, db) { // методы post/get
 
 	app.get('/', (req, res, next) => {
 		res.send(message);
+		next();
 	});
 
 	app.post('/books', (req, res, next) => { // добавление книги
@@ -55,132 +56,157 @@ module.exports = function (app, db) { // методы post/get
 			year: year_book
 		};
 
-		collectionBook.insertOne(book, function (err, result) {
+		collectionBook.insertOne(book, (err, result) => {
 			if (err)
 				return next(new errs.BadGatewayError(err.message));
 			console.log(result.ops);
 		});
 
-		res.send("Book successfully added");
+		let resBody = {
+			code: 'Status ok',
+			message: 'Book successfully added'
+		};
+
+		res.send(resBody);
+		next();
 	});
 
 	app.get('/books/:id', (req, res, next) => { // получить информацию о книге по id
-	   // const book_id = req.params.id;
+		// const book_id = req.params.id;
 		if (!isset(req.params.id))
 			return next(new errs.InvalidArgumentError("Not enough parameters: mast be (id)"));
+		if (!ObjectID.isValid(req.params.id))
+			return next(new errs.InvalidArgumentError("Incorrect id"));
 
-		try {
-			const idBook = ObjectID(req.params.id);
+		const idBook = ObjectID(req.params.id);
 
-            collectionBook.find({"_id": idBook}).toArray(function (err, info) {
-                if (err)
-                    return next(new errs.BadGatewayError(err.message));
-                if(info.length === 0)
-                    return next(new errs.InvalidArgumentError("Not found"));
-
-                let query = {book_id: ObjectID(idBook)};
-
-                collectionBooking.find(query).sort({taken: -1}).toArray(function (err, result) {
-                    if (err)
-                        return next(new errs.BadGatewayError(err.message));
-
-					let bookInfo = {};
-                    bookInfo['book'] = info[0];
-                    bookInfo['lastBooking'] = result.length === 0 ? {} : result[0];
-
-                    res.send(bookInfo);
-                });
-            });
-		} catch(err) {
-			return next(new errs.InvalidArgumentError(err.message));
+		let queryIdBook = {
+			'_id': idBook
 		}
+
+		collectionBook.find(queryIdBook).toArray((err, info) => {
+			if (err)
+				return next(new errs.BadGatewayError(err.message));
+			if(info.length === 0)
+				return next(new errs.InvalidArgumentError("Not found"));
+
+			let query = {
+				book_id: idBook
+			};
+
+			collectionBooking.find(query).sort({taken: -1}).toArray((err, result) => {
+				if (err)
+					return next(new errs.BadGatewayError(err.message));
+
+				let bookInfo = {};
+				bookInfo['book'] = info[0];
+				bookInfo['lastBooking'] = result.length === 0 ? {} : result[0];
+
+				res.send(bookInfo);
+				next();
+			});
+		});
 	});
 
 	app.post('/booking', (req, res, next) => { // забронировать книгу
-
 		if (!isset(req.body.id))
 			return next(new errs.InvalidArgumentError("Not enough body data: mast be (id)"));
 		if (!isset(req.body.name))
 			return next(new errs.InvalidArgumentError("Not enough body data: mast be (name)"));
+		if (!ObjectID.isValid(req.params.id))
+			return next(new errs.InvalidArgumentError("Incorrect id"));
 
-		try {
-			const book_id = ObjectID(req.body.id);
-            collectionBook.find({"_id":  book_id}).toArray(function (err, result){
-                if (result.length === 0)
-                    return next(new errs.InvalidArgumentError("not found"));
-                if (result[0]['available'] === false)
-				{
-                    return next(new errs.InvalidArgumentError("The book is already booked"));
-				}
+		const book_id = ObjectID(req.body.id);
 
-                let booking = {
-                    book_id: book_id,
-                    user: req.body.name,
-                    taken: new Date(),
-                    returned: null
-                };
-
-                collectionBooking.insertOne(booking, function (err) {
-                    if (err)
-                        return next(new errs.BadGatewayError(err.message));
-                });
-
-                let query = {_id: book_id};
-                let values = {$set: {available: false}};
-
-                collectionBook.updateOne(query, values, function (err) {
-                    if (err)
-                        return next(new errs.BadGatewayError(err.message));
-                    res.send("Book successfully booked");
-                });
-            });
-		} catch(err) {
-			return next(new errs.InvalidArgumentError(err.message));
+		let queryIdBook = {
+			'_id': book_id
 		}
+
+		collectionBook.find(queryIdBook).toArray((err, result) => {
+			if (result.length === 0)
+				return next(new errs.InvalidArgumentError("not found"));
+			if (result[0]['available'] === false)
+				return next(new errs.InvalidArgumentError("The book is already booked"));
+
+			let booking = {
+				book_id: book_id,
+				user: req.body.name,
+				taken: new Date(),
+				returned: null
+			};
+
+			collectionBooking.insertOne(booking, (err) => {
+				if (err)
+					return next(new errs.BadGatewayError(err.message));
+			});
+
+			let query = {_id: book_id};
+			let values = {$set: {available: false}};
+
+			collectionBook.updateOne(query, values, (err) => {
+				if (err)
+					return next(new errs.BadGatewayError(err.message));
+
+				let resBody = {
+					code: 'Status ok',
+					message: 'Book successfully booked'
+				};
+
+				res.send(resBody);
+				next();
+			});
+		});
 	});
 
-    app.post('/cancelBooking', (req, res, next) => { //снять бронь
+	app.post('/cancelBooking', (req, res, next) => { //снять бронь
+		if (!isset(req.body.id))
+			return next(new errs.InvalidArgumentError("Not enough body data: mast be (id)"));
+		if (!ObjectID.isValid(req.params.id))
+			return next(new errs.InvalidArgumentError("Incorrect id"));
 
-        if (!isset(req.body.id))
-            return next(new errs.InvalidArgumentError("Not enough body data: mast be (id)"));
+		const book_id = ObjectID(req.body.id);
 
-        try {
-            const book_id = ObjectID(req.body.id);
+		let query = {
+			book_id: book_id
+		};
 
-            let query = {book_id: book_id};
+		collectionBooking.find(query).sort({taken: -1}).toArray((err, result) => {
+			if (err)
+				return next(new errs.BadGatewayError(err.message));
+			if(result.length === 0)
+				return next(new errs.InvalidArgumentError("Not found"));
 
-            collectionBooking.find(query).sort({taken: -1}).toArray(function (err, result) {
-                if (err || result.length === 0)
-                    return next(new errs.BadGatewayError(err.message));
+			let info = result[0];
+			if (info['returned'] !== null)
+				return next(new errs.InvalidArgumentError("book is not booked"));
 
-                let info = result[0];
-                if (info['returned'] !== null)
-                    return next(new errs.InvalidArgumentError("book is not booked"));
+			let query = {_id: book_id};
+			let values = {$set: {available: true}};
 
-                let query = {_id: book_id};
-                let values = {$set: {available: true}};
+			collectionBook.updateOne(query, values, (err) => {
+				if (err)
+					return next(new errs.BadGatewayError(err.message));
+			});
 
-                collectionBook.updateOne(query, values, function (err) {
-                    if (err)
-                        return next(new errs.BadGatewayError(err.message));
-                });
+			let idBooking = result[0]._id;
 
-                let idBooking = result[0]._id;
+			query = {_id: idBooking};
+			values = {$set: {returned: new Date()}};
 
-                query = {_id: idBooking};
-                values = {$set: {returned: new Date()}};
+			collectionBooking.updateOne(query, values, (err) => {
+				if (err)
+					return next(new errs.BadGatewayError(err.message));
+			});
 
-                collectionBooking.updateOne(query, values, function (err) {
-                    if (err)
-                        return next(new errs.BadGatewayError(err.message));
-                });
+			let resBody = {
+				code: 'Status ok',
+				message: 'Book was returned'
+			};
 
-                res.send("Book was returned");
-            });
-        } catch(err) {
-            return next(new errs.InvalidArgumentError(err.message));
-        }
-    });
+			res.send(resBody);
+			next();
+		});
+	});
 
 
 	app.get('/books/showPage/:numPage', (req, res, next) => { // пейджинг
@@ -195,26 +221,30 @@ module.exports = function (app, db) { // методы post/get
 		if (numPage < 1)
 			return next(new errs.InvalidArgumentError("Argument less 1"));
 
-		collectionBook.find().limit(countBook).skip(countBook * (numPage - 1)).toArray(function (err, results) {
+		collectionBook.find().limit(countBook).skip(countBook * (numPage - 1)).toArray((err, results) => {
 			if (err)
 				return next(new errs.BadGatewayError(err.message));
 			res.send(results);
+			next();
 		});
 	});
 
 	app.get('/books/searchBook', (req, res, next) => { // поиск по подстроке (фильтрация по названию)
 		if (!isset(req.query.substring))
 			return next(new errs.InvalidArgumentError("Not enough query parameters: mast be (substring)"));
-
+		if (isEmpty(req.query.substring))
+			return next(new errs.InvalidArgumentError("Substring is empty"));
+		
 		let substring = req.query.substring;
 		let query = {
 			"name": {$regex: substring}
 		};
 
-		collectionBook.find(query).toArray(function (err, result) {
+		collectionBook.find(query).toArray((err, result) => {
 			if (err)
 				return next(new errs.BadGatewayError(err.message));
 			res.send(result);
+			next();
 		});
 	});
 
@@ -226,10 +256,11 @@ module.exports = function (app, db) { // методы post/get
 		let isAvailable = (available === 'true'); // прости, но ты пишешь на javascript
 		let query = (available ? {"available": isAvailable} : {});
 
-		collectionBook.find(query).toArray(function (err, result) {
+		collectionBook.find(query).toArray((err, result) => {
 			if (err)
 				return next(new errs.BadGatewayError(err.message));
 			res.send(result);
+			next();
 		});
 	});
 };
