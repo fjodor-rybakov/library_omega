0097fc458bb0807906d752a8498673df893090fd
fjodor-rybakov <fedryb@mail.ru>
2018-07-03 13:37:34 +0300

    add test empty substr

--- test/appTests.js
+++ test/appTests.js
@@ -4,32 +4,32 @@ var assert = require('assert');
 var url = 'localhost:' + 3000;
 
 describe('add book', function () {
-    it('mustAdd', function (done) {
-        supertest(url)
-            .post('/books')
-            .send({
-                name: 'john',
-                link: 'link',
-                authors: 'author_name',
-                description: 'description',
-                year: 2000
-            })
-            .set('Content-Type', 'application/json')
-            .set('Accept', 'application/json')
-            .expect(200, function (err, result) {
-                if (err) {
-                    console.log(err);
-                    console.log(JSON.stringify(res));
-                    throw err;
-                }
-                done();
-            });
-    });
+	it('mustAdd', function (done) {
+		supertest(url)
+			.post('/books')
+			.send({
+				name: 'john',
+				link: 'link',
+				authors: 'author_name',
+				description: 'description',
+				year: 2000
+			})
+			.set('Content-Type', 'application/json')
+			.set('Accept', 'application/json')
+			.expect(200, function (err, result) {
+				if (err) {
+					console.log(err);
+					console.log(JSON.stringify(res));
+					throw err;
+				}
+				done();
+			});
+	});
 });
 
 describe('get book info', function () {
 	it('getInfoInJSON', function (done) {
-			supertest(url)
+		supertest(url)
 			.get('/books/5b39040051c1b60d14738c7a')
 			.expect('Content-Type', /json/)
 			.expect(200, function (err,res) {
@@ -45,7 +45,7 @@ describe('get book info', function () {
 
 describe('paging books', function () {
 	it('paging', function (done) {
-			supertest(url)
+		supertest(url)
 			.get('/books/showPage/1')
 			.expect('Content-Type', /json/)
 			.expect(200, function (err,res) {
@@ -61,7 +61,7 @@ describe('paging books', function () {
 
 describe('search substring books', function() {
 	it('search substring', function(done) {
-			supertest(url)
+		supertest(url)
 			.get('/books/searchBook')
 			.query({ substring: 'Б' })
 			.expect('Content-Type', /json/)
@@ -74,11 +74,26 @@ describe('search substring books', function() {
 				done();
 			});
 	});
+
+	it('search empty substring', function(done) {
+		supertest(url)
+			.get('/books/searchBook')
+			.query({ substring: '' })
+			.expect('Content-Type', /json/)
+			.expect(409, function (err,res) {
+				if (err) {
+					console.log(err);
+					console.log(JSON.stringify(res));
+					throw err;
+				}
+				done();
+			});
+	});
 });
 
 describe('filter status books', function() {
 	it('filter status', function(done) {
-			supertest(url)
+		supertest(url)
 			.get('/books')
 			.query({ available: true })
 			.expect('Content-Type', /json/)
@@ -94,55 +109,55 @@ describe('filter status books', function() {
 });
 
 describe('booking', function () {
-    it('getInfoInJSON', function (done) {
-        supertest(url)
-            .post('/booking')
-            .send({
-                name: "john",
-                id: "5b39038651c1b60d14738c79"
-            })
-            .expect(200, function (err, res) {
-                if (err) {
-                    console.log(err);
-                    console.log(JSON.stringify(res));
-                    throw err;
-                }
-                done();
-            });
-    });
+	it('getInfoInJSON', function (done) {
+		supertest(url)
+			.post('/booking')
+			.send({
+				name: "john",
+				id: "5b39038651c1b60d14738c79"
+			})
+			.expect(200, function (err, res) {
+				if (err) {
+					console.log(err);
+					console.log(JSON.stringify(res));
+					throw err;
+				}
+				done();
+			});
+	});
 
-    it('checkBooking', function (done) {
-        supertest(url)
-            .get('/books/5b39038651c1b60d14738c79')
-            .expect('Content-Type', /json/)
-            .expect(200, function (err, res) {
-                var data = JSON.parse(res.text);
-                if (err) {
-                    console.log(err);
-                    console.log(JSON.stringify(res));
-                    throw err;
-                }
-                assert.strictEqual(data.book.available, false);
-                done();
-            });
-    });
+	it('checkBooking', function (done) {
+		supertest(url)
+			.get('/books/5b39038651c1b60d14738c79')
+			.expect('Content-Type', /json/)
+			.expect(200, function (err, res) {
+				var data = JSON.parse(res.text);
+				if (err) {
+					console.log(err);
+					console.log(JSON.stringify(res));
+					throw err;
+				}
+				assert.strictEqual(data.book.available, false);
+				done();
+			});
+	});
 });
 
 describe('cancelBooking', function () {
-    it('cancelBooking', function (done) {
-        supertest(url)
-            .post('/cancelBooking')
-            .send({
-                id: "5b39038651c1b60d14738c79"
-            })
-            .expect(200, function (err, res) {
-                //  var data = JSON.parse(res);
-                if (err) {
-                    console.log(err);
-                    console.log(JSON.stringify(res));
-                    throw err;
-                }
-                done();
-            })
-    })
+	it('cancelBooking', function (done) {
+		supertest(url)
+			.post('/cancelBooking')
+			.send({
+				id: "5b39038651c1b60d14738c79"
+			})
+			.expect(200, function (err, res) {
+				//  var data = JSON.parse(res);
+				if (err) {
+					console.log(err);
+					console.log(JSON.stringify(res));
+					throw err;
+				}
+				done();
+			})
+	})
 });
